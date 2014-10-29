8aee559e006fdaf75414689b38379db796fc19c4
Jeromy Tompkins <tompkins@nscl.msu.edu>
2014-10-28 22:37:35 -0400

    Adds proper licensing to all of files

--- s800tstamp/s800timestamptests.cpp
+++ s800tstamp/s800timestamptests.cpp
@@ -1,6 +1,20 @@
+//
+//    This software is Copyright by the Board of Trustees of Michigan
+//    State University (c) Copyright 2014.
+//
+//    You may use this software under the terms of the GNU public license
+//    (GPL).  The terms of this license are described at:
+//
+//     http://www.gnu.org/licenses/gpl.txt
+//
+//     Author:
+//       Jeromy Tompkins
+//	     NSCL
+//	     Michigan State University
+//	     East Lansing, MI 48824-1321
+
 // s800timestamptests.cpp
 //
-// Author : Jeromy Tompkins
 // Date : 2/21/2014
 //
 // Tests to ensure proper extraction of an s800 timestamp
